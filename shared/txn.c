9c6738efcc61ead98974fc28bc78020672e7cfd0
Zach Brown <zab@zabbo.net>
2024-12-20 14:37:52 -0800

    Update txn to use retries
    The first pass at multi-block transactions were built around first
    returning errors while preparing references to all the modified blocks,
    then making modifications to all the blocks which could not fail.  This
    is all well and good, but getting it going with modifications like
    inserting and deleting variable size btree items, which can split and
    merge blocks, was wandering squarely into uncomfortably complex
    territory.
    
    This changes the model to incrementally acquire block references and
    modifying the blocks as they're acquired.  This model, combined with the
    block-granular cache coherency protocols, can lead to deadlocks.  This
    is built around using trylocks when getting block references out of
    order.  To that end, modifications to blocks first record relatively
    fine grained per-block undo logs.  We can always undo all stores to
    block contents to unwind errors without needing logical error unwinding
    paths to match all the modifications in each transaction.
    
    A lot of the mechanisms are still stubbed out or nonexistant, but this
    is a step in that direction.
    
    Signed-off-by: Zach Brown <zab@versity.com>

--- shared/txn.c
+++ shared/txn.c
@@ -1,144 +1,338 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
+#include "shared/lk/atomic.h"
+#include "shared/lk/bug.h"
 #include "shared/lk/err.h"
 #include "shared/lk/errno.h"
 #include "shared/lk/list.h"
 
 #include "shared/block.h"
+#include "shared/compare.h"
+#include "shared/format-block.h"
 #include "shared/txn.h"
+#include "shared/unbuf.h"
 
-/*
- * Callers describe multi-block transactions as a set of blocks with
- * access modes.  As we try to acquire access to each block we call
- * ->prepare so that it can access the block contents and perhaps add
- * more blocks.  Once all blocks are prepared we call ->commit on all
- * the blocks.
- *
- * This frees the caller from needing to worry about the access
- * acquisition rules.  They assemble the blocks as they see fit and we
- * ensure that we safely acquire access to them without deadlocking.
- *
- * The commit functions make changes to blocks once prepare has ensured
- * that all the changes will succeed.  This avoids unwinding in the face
- * of error.  We work with the block cache to ensure that the blocks are
- * written as an atomic unit as well.
- */
-
-struct ngnfs_transaction_block {
-	struct list_head head;
+struct ngnfs_txn_block {
+	struct rb_node node;
 	struct list_head write_head;
 	struct ngnfs_block *bl;
+	struct ngnfs_undo_buf *unbuf;
 	u64 bnr;
-	nbf_t nbf;
-	txn_prepare_fn prepare;
-	txn_commit_fn commit;
-	void *arg;
+	nbf_t requested_nbf;
+	nbf_t acquired_nbf;
+	bool deadlocked;
 };
 
-/* off = bl - head -> bl = head + off */
-#define WRITE_HEAD_BL_OFFSET \
-	(ssize_t)(offsetof(struct ngnfs_transaction_block, bl) - \
-		  offsetof(struct ngnfs_transaction_block, write_head))
+struct rb_insert_args {
+	struct rb_node *parent;
+	struct rb_node **link;
+};
 
-void ngnfs_txn_init(struct ngnfs_transaction *txn)
+static struct ngnfs_txn_block *find_tblk(struct rb_root *root, u64 bnr, struct rb_insert_args *ins)
 {
-	INIT_LIST_HEAD(&txn->blocks);
-	INIT_LIST_HEAD(&txn->writes);
+	struct ngnfs_txn_block *tblk;
+	int cmp;
+
+	ins->parent = NULL;
+	ins->link = &root->rb_node;
+
+	while (*ins->link) {
+		ins->parent = *ins->link;
+		tblk = container_of(*ins->link, struct ngnfs_txn_block, node);
+
+		cmp = ngnfs_compare(bnr, tblk->bnr);
+		if (cmp == 0)
+			return tblk;
+
+		if (cmp < 0)
+			ins->link = &(*ins->link)->rb_left;
+		else
+			ins->link = &(*ins->link)->rb_right;
+	}
+
+	return NULL;
 }
 
-/*
- * In theory it's legitimate to implement a "transaction" by just
- * acquiring a mix of read and write access on blocks without doing
- * anything, so we don't enforce that prepare or commit are non-null.
- */
-int ngnfs_txn_add_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn, u64 bnr,
-			nbf_t nbf, txn_prepare_fn prepare, txn_commit_fn commit, void *arg)
+static struct ngnfs_txn_block *containing_tblk(struct rb_node *node)
 {
-	struct ngnfs_transaction_block *tblk;
+	return node ? container_of(node, struct ngnfs_txn_block, node) : NULL;
+}
+
+static struct ngnfs_txn_block *last_tblk(struct rb_root *root)
+{
+	return containing_tblk(rb_last(root));
+}
+
+static struct ngnfs_txn_block *next_tblk(struct ngnfs_txn_block *tblk)
+{
+	return containing_tblk(tblk ? rb_next(&tblk->node) : NULL);
+}
+
+static struct ngnfs_txn_block *prev_tblk(struct ngnfs_txn_block *tblk)
+{
+	return containing_tblk(tblk ? rb_prev(&tblk->node) : NULL);
+}
+
+static void put_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
+		      struct ngnfs_txn_block *tblk, nbf_t nbf)
+{
+	nbf |= tblk->acquired_nbf;
+
+	if ((nbf & (NBF_READ|NBF_WRITE)) == 0)
+		return;
+
+	if (nbf & NBF_WRITE)
+		list_del_init(&tblk->write_head);
+
+	ngnfs_block_put(nfi, tblk->bl, nbf);
+
+	tblk->bl = NULL;
+	tblk->acquired_nbf = 0;
+}
+
+static int get_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
+		     struct ngnfs_txn_block *tblk, nbf_t nbf)
+{
+	struct ngnfs_block *bl;
 	int ret;
 
-	tblk = kmalloc(sizeof(struct ngnfs_transaction_block), GFP_NOFS);
-	if (!tblk) {
-		ret = -ENOMEM;
+	/* we want to retry requested mode if it fails */
+	tblk->requested_nbf = nbf & (NBF_READ|NBF_WRITE);
+
+	/* always wait for dirty to fall under the limit before we start writing */
+	if ((nbf & NBF_WRITE) && list_empty(&txn->writes))
+		ngnfs_block_dirty_limit_wait(nfi);
+
+	bl = ngnfs_block_get(nfi, tblk->bnr, nbf);
+	if (IS_ERR(bl)) {
+		ret = PTR_ERR(bl);
+		if (ret == -EDEADLK)
+			tblk->deadlocked = true;
 		goto out;
 	}
 
-	INIT_LIST_HEAD(&tblk->write_head);
-	tblk->bl = NULL;
-	tblk->bnr = bnr;
-	tblk->nbf = nbf;
-	tblk->prepare = prepare;
-	tblk->commit = commit;
-	tblk->arg = arg;
+	tblk->bl = bl;
+	tblk->acquired_nbf = tblk->requested_nbf;
 
-	list_add_tail(&tblk->head, &txn->blocks);
+	if (nbf & NBF_WRITE)
+		list_add_tail(&tblk->write_head, &txn->writes);
+
+	if (!tblk->unbuf) {
+		ret = ngnfs_unbuf_alloc(ngnfs_block_buf(bl), NGNFS_BLOCK_SIZE, &tblk->unbuf);
+		if (ret < 0) {
+			put_block(nfi, txn, tblk, 0);
+			goto out;
+		}
+	}
+
 	ret = 0;
 out:
 	return ret;
 }
 
 /*
- * Callers are responsible for tearing down the txn.
+ * Write references can satisfy both, otherwise reads must match.
  */
-int ngnfs_txn_execute(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn)
+static bool nbf_rw_compatible(nbf_t have, nbf_t want)
 {
-	struct ngnfs_transaction_block *tblk;
-	struct ngnfs_block *bl;
-	int ret = 0;
+	return (have & NBF_WRITE) || (have & want & NBF_READ);
+}
 
-	list_for_each_entry(tblk, &txn->blocks, head) {
-		bl = ngnfs_block_get(nfi, tblk->bnr, tblk->nbf);
-		if (IS_ERR(bl)) {
-			ret = PTR_ERR(bl);
+/*
+ * Get a shared read or exclusive write reference to blocks as part of a
+ * transaction.
+ *
+ * Callers request blocks in whatever traversal order they happen to
+ * implement.  We define ordered acquisition as ascending block number.
+ * The transaction notices when a reference is requested out of order
+ * and attempts a trylock, returning an indication to retry if blocking
+ * could deadlock.
+ *
+ * While most transactions will reference a reasonably small number of
+ * blocks, there are a few transactions have an exceptional amount.  In
+ * particular, renames across directories can read thousands of parent
+ * dir blocks for particularly pathological dir trees.
+ */
+int ngnfs_txn_get_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
+			u64 bnr, nbf_t nbf, struct ngnfs_txn_block **tblk_ret, void **data_ret)
+{
+	struct ngnfs_txn_block *tblk;
+	struct rb_insert_args ins;
+	void *data;
+	int ret;
+
+	/*
+	 * Our callers specify very few flags, the rest we generate when calling the cache.
+	 */
+	if (WARN_ON_ONCE(nbf & ~(NBF_READ|NBF_WRITE|NBF_TRY))) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	tblk = find_tblk(&txn->blocks, bnr, &ins);
+	if (!tblk) {
+		tblk = kmalloc(sizeof(struct ngnfs_txn_block), GFP_NOFS);
+		if (!tblk) {
+			ret = -ENOMEM;
 			goto out;
 		}
 
-		tblk->bl = bl;
+		RB_CLEAR_NODE(&tblk->node);
+		INIT_LIST_HEAD(&tblk->write_head);
+		tblk->bl = NULL;
+		tblk->unbuf = NULL;
+		tblk->bnr = bnr;
+		tblk->requested_nbf = 0;
+		tblk->acquired_nbf = 0;
 
-		if (tblk->prepare) {
-			ret = tblk->prepare(nfi, txn, tblk->bl, tblk->arg);
-			if (ret < 0)
-				goto out;
-		}
-
-		if (tblk->nbf & NBF_WRITE)
-			list_add_tail(&tblk->write_head, &txn->writes);
+		rb_link_node(&tblk->node, ins.parent, ins.link);
+		rb_insert_color(&tblk->node, &txn->blocks);
 	}
 
-	if (!list_empty(&txn->writes)) {
-		ret = ngnfs_block_dirty_begin(nfi, &txn->writes, WRITE_HEAD_BL_OFFSET);
-		if (ret < 0)
-			goto out;
-
-		list_for_each_entry(tblk, &txn->writes, write_head) {
-			if (tblk->commit)
-				tblk->commit(nfi, txn, tblk->bl, tblk->arg);
-		}
-
-		ngnfs_block_dirty_end(nfi, &txn->writes, WRITE_HEAD_BL_OFFSET);
+	/* done if we already have a compatible reference */
+	if (nbf_rw_compatible(tblk->acquired_nbf, nbf)) {
+		ret = 0;
+		goto out;
 	}
 
+	/*
+	 * txn callers can request a write reference to a block that
+	 * they have a read reference to.  (retry will never do this).
+	 * The block cache will do its best to satisfy the request but
+	 * might not be able to.  If it fails we'll get a write
+	 * reference as we retry.
+	 */
+	if ((nbf & NBF_WRITE) && (tblk->acquired_nbf & NBF_READ))
+		nbf |= NBF_CONVERT_WRITE;
+
+	ret = get_block(nfi, txn, tblk, nbf);
 out:
+	if (ret < 0) {
+		tblk = NULL;
+		data = NULL;
+	} else {
+		data = ngnfs_block_buf(tblk->bl);
+	}
+
+	if (tblk_ret)
+		*tblk_ret = tblk;
+	if (data_ret)
+		*data_ret = data;
+
 	return ret;
 }
 
 /*
- * Tear down a transaction.  The transaction must have been initialized
- * and this can be called for any state of the transaction, including
- * repeatedly.  It is a nop on a newly initialized or previously
- * destroyed txn.  The caller is responsible for the allocation of the
- * txn struct itself.
+ * Transactions maintain an allocation cursor.  It tracks allocation
+ * blocks with available blocks that it has write access to.  Our
+ * transaction model allows an allocation to use frees within the
+ * allocation so we may want to track blocks that we free into here as
+ * well.
+ *
+ * XXX except that allocation blocks don't exist and we're not doing any
+ * of that :).  Each new mount will start allocating from the same
+ * global fake block number and will overwrite anything that was written
+ * by previous mounts.
  */
-void ngnfs_txn_destroy(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn)
-{
-	struct ngnfs_transaction_block *tblk;
-	struct ngnfs_transaction_block *tmp;
+static atomic64_t global_fake_alloc = ATOMIC64_INIT(NGNFS_ROOT_INO + 1);
 
-	list_for_each_entry_safe(tblk, tmp, &txn->blocks, head) {
-		if (!list_empty(&tblk->write_head))
-			list_del_init(&tblk->write_head);
-		list_del_init(&tblk->head);
-		ngnfs_block_put(tblk->bl);
-		kfree(tblk);
+int ngnfs_txn_alloc_meta(struct ngnfs_transaction *txn, u64 *bnr_ret)
+{
+	*bnr_ret = atomic64_inc_return(&global_fake_alloc);
+	return 0;
+}
+
+/*
+ * A caller with a transaction is about to modify a region of block
+ * contents.  Use the block's undo buffer to save away the original
+ * contents in case we need to restore it.
+ */
+void ngnfs_txn_save(struct ngnfs_txn_block *tblk, void *ptr, size_t size)
+{
+	ngnfs_unbuf_save(tblk->unbuf, ptr, size);
+}
+
+static void free_tblk(struct ngnfs_transaction *txn, struct ngnfs_txn_block *tblk, bool erase)
+{
+	BUG_ON(tblk->acquired_nbf & (NBF_READ|NBF_WRITE));
+
+	if (erase && !RB_EMPTY_NODE(&tblk->node))
+		rb_erase(&tblk->node, &txn->blocks);
+	if (!list_empty(&tblk->write_head))
+		list_del_init(&tblk->write_head);
+	ngnfs_unbuf_free(tblk->unbuf);
+
+	kfree(tblk);
+}
+
+/*
+ * Transaction callers call this as their loop body finishes.  On
+ * success (ret >= 0) we do nothing and don't have the loop retry.  On
+ * all errors we'll undo the modification of block contents made by the
+ * transaction.
+ *
+ * If we're returning a hard error then we let teardown free the
+ * transaction as efficiently as possible.
+ *
+ * If the error indicates that we should retry then we more carefully
+ * release block references to the point of deadlock, then block
+ * reacquiring references in order before the caller retries.
+ *
+ * (XXX we'll want to mark blocks that were not found by retries so we
+ * can just drop them.)
+ */
+bool ngnfs_txn_retry(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn, int *ret)
+{
+	struct ngnfs_txn_block *tblk;
+	bool retry = false;
+
+	if (*ret >= 0)
+		goto out;
+
+	/* undo block modifications on error */
+	list_for_each_entry(tblk, &txn->writes, write_head)
+		ngnfs_unbuf_restore(tblk->unbuf);
+
+	if (*ret != -EDEADLK)
+		goto out;
+
+	/* put references to all blocks after the bnr that failed */
+	for (tblk = last_tblk(&txn->blocks); tblk; tblk = prev_tblk(tblk)) {
+
+		put_block(nfi, txn, tblk, NBF_NODIRTY);
+
+		if (tblk->deadlocked) {
+			tblk->deadlocked = false;
+			break;
+		}
 	}
+
+	/* get blocking references in order from the failed bnr  */
+	for (; tblk; tblk = next_tblk(tblk)) {
+		*ret = get_block(nfi, txn, tblk, tblk->requested_nbf);
+		if (*ret < 0)
+			goto out;
+	}
+
+	*ret = 0;
+	retry = true;
+out:
+	return retry;
+}
+
+/*
+ * Free all the resources in the txn so that it is safe for the caller
+ * to free the txn struct memory itself, or perhaps reuse it.
+ */
+void ngnfs_txn_teardown(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn)
+{
+	struct ngnfs_txn_block *tblk;
+	struct ngnfs_txn_block *tmp;
+
+	rbtree_postorder_for_each_entry_safe(tblk, tmp, &txn->blocks, node) {
+		put_block(nfi, txn, tblk, 0);
+		free_tblk(txn, tblk, false);
+	}
+
+	txn->blocks = RB_ROOT;
 }
--- shared/txn.h
+++ shared/txn.h
@@ -4,36 +4,100 @@
 
 struct ngnfs_transaction;
 
-#include "shared/block.h"
+#include "shared/lk/byteorder.h"
 #include "shared/lk/list.h"
+#include "shared/lk/rbtree.h"
+
+#include "shared/block.h"
+#include "shared/unbuf.h"
+
+struct ngnfs_txn_block;
+
+#define ngnfs_tblk_assign(tblk, lhs, rhs)			\
+do {								\
+	__typeof__(lhs) *lhs_ = &(lhs);				\
+								\
+	ngnfs_txn_save(tblk, lhs_, sizeof(*lhs_));		\
+	*lhs_ = (rhs);						\
+} while (0)							\
+
+#define ngnfs_tblk_memcpy(tblk, dst, src, n)			\
+do {								\
+	__typeof__(dst) dst_ = (dst);				\
+	__typeof__(n) n_ = (n);					\
+								\
+	ngnfs_txn_save(tblk, dst_, n_);				\
+	memcpy(dst_, (src), n_);				\
+} while (0)
+
+#define ngnfs_tblk_memset(tblk, s, c, n)			\
+do {								\
+	__typeof__(s) s_ = (s);					\
+	__typeof__(n) n_ = (n);					\
+								\
+	ngnfs_txn_save(tblk, s_, n_);				\
+	memset(s_, (c), n_);					\
+} while (0)
+
+#define ngnfs_tblk_memmove(tblk, dst, src, n)			\
+do {								\
+	__typeof__(dst) dst_ = (dst);				\
+	__typeof__(n) n_ = (n);					\
+								\
+	ngnfs_txn_save(tblk, dst_, n_);				\
+	memmove(dst_, (src), n_);				\
+} while (0)
+
+#define ngnfs_tblk_le16_add_cpu(tblk, lep, val)			\
+do {								\
+	__typeof__(lep) lep_ = (lep);				\
+								\
+	ngnfs_txn_save(tblk, lep_, sizeof(*lep_));		\
+	le16_add_cpu(lep_, (val));				\
+} while (0)
+
+/*
+ * Given a (buf) buffer of (total) size, zero the tail bytes	\
+ * after (head) initial bytes.					\
+ * skipping (head) initial bytes.
+ */
+#define ngnfs_tblk_zero_tail(tblk, buf, head, total)		\
+do {								\
+	__typeof__(head) head_ = (head);			\
+	__typeof__(head) tail_ = (total) - head_;		\
+								\
+	if (tail_ > 0)						\
+		ngnfs_tblk_memset((tblk), (void *)(buf) + head_, 0, tail_); \
+} while (0)
 
 /*
  * We expose the type so callers can allocate and initialize it, but they don't
  * use it directly.
  */
 struct ngnfs_transaction {
-	struct list_head blocks;
+	struct rb_root blocks;
 	struct list_head writes;
+	bool retry_triggered;
 };
 
 #define INIT_NGNFS_TXN(txn) {				\
-	.blocks = LIST_HEAD_INIT(txn.blocks),		\
+	.blocks = RB_ROOT,				\
 	.writes = LIST_HEAD_INIT(txn.writes),		\
+	.retry_triggered = false,			\
 }
 
-typedef int (*txn_prepare_fn)(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
-			      struct ngnfs_block *bl, void *arg);
-/*
- * Commit functions should be quick and can not fail.  Prepare's job is
- * to ensure that commit can proceed or return errors.
- */
-typedef void (*txn_commit_fn)(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
-			      struct ngnfs_block *bl, void *arg);
+static inline void ngnfs_txn_init(struct ngnfs_transaction *txn)
+{
+	txn->blocks = RB_ROOT;
+	INIT_LIST_HEAD(&txn->writes);
+	txn->retry_triggered = false;
+}
 
-void ngnfs_txn_init(struct ngnfs_transaction *txn);
-int ngnfs_txn_add_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn, u64 bnr,
-			nbf_t nbf, txn_prepare_fn prepare, txn_commit_fn commit, void *arg);
-int ngnfs_txn_execute(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn);
-void ngnfs_txn_destroy(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn);
+int ngnfs_txn_get_block(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn,
+			u64 bnr, nbf_t nbf, struct ngnfs_txn_block **tblk_ret, void **data_ret);
+int ngnfs_txn_alloc_meta(struct ngnfs_transaction *txn, u64 *bnr_ret);
+void ngnfs_txn_save(struct ngnfs_txn_block *tblk, void *ptr, size_t size);
+bool ngnfs_txn_retry(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn, int *ret);
+void ngnfs_txn_teardown(struct ngnfs_fs_info *nfi, struct ngnfs_transaction *txn);
 
 #endif
